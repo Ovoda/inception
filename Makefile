bb3922edc78914beeeef8f595b5f8e1930bf3f08
user42 <Ovoda>
2021-05-26 12:22:02 +0200

    nginx linked to phpfpm, started mariadb

--- Makefile
+++ Makefile
@@ -1,1 +1,11 @@
-all: echo "make"
+all:
+	docker-compose -f ./srcs/docker-compose.yml up -d --build 
+clean_i:
+	docker rmi $(docker images -a)
+
+clean_c:
+	docker rm $(docker ps -aq)
+
+clean: clean_i clean_c
+
+re: clean all
--- srcs/services/wp-php-fpm/start.sh
+++
@@ -1,8 +0,0 @@
-cp php-fpm.conf /etc/php/7.3/fpm/php-fpm.conf
-apt isntall -y vim
-/etc/init.d/php7.3-fpm start
-/etc/init.d/php7.3-fpm reload 
-/etc/init.d/php7.3-fpm status
-service --status-all
-tail -f /var/log/php7.3-fpm.log
-tail -f
